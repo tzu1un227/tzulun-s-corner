548d52f5ff5f67eb86312db4c795091bfbb72f7d
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2024-09-11 22:58:54 +0800

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,9 +1,9 @@
 import os
 os.system('cls')
 socialMedia='facebook'
-Audience='一般大眾'
-style='感性'
-intro=''
+Audience='上班族'
+style='嚴謹'
+intro='(產品介紹)'
 
 socialMedia_dict={'facebook':[450,50],'LINE':[100,20],'Instagram':[200,50]}
 Audience_dict={'一般大眾':'加入一些諧音笑話','上班族':'穿插一些英文單字','狀老年':'使用一些閩南語','學生':'使用一些網路用語'}
@@ -26,6 +26,6 @@ style_dict={'感性':'正向、友善且溫暖，用字中性且擅長情境描�
 '''
 
 
-prompt2=f'{intro}請基於上述的產品介紹，邀請大家使用此產品，如有連結鼓勵點擊，文末鼓勵按讚留言分享 平台是{socialMedia}，把字數控制在 {socialMedia_dict[socialMedia][0]} 字以內，並在前 {socialMedia_dict[socialMedia][1]} 字有吸引人的標題 受眾是{Audience}，在廣告中{Audience_dict[Audience]} 廣告風格是{style}，以{style_dict[style]}的方式產生出廣告'
+prompt2=f'請你幫我產生出一則廣告\n{intro}\n以上是產品的介紹\n目標平台是{socialMedia}，把字數控制在 {socialMedia_dict[socialMedia][0]} 字以內，並在前 {socialMedia_dict[socialMedia][1]} 字內含有吸引人的標題\n受眾是{Audience}，在廣告中{Audience_dict[Audience]}\n廣告風格是{style}，以{style_dict[style]}的方式產生出廣告\n請基於上述的條件，產生廣告邀請大家使用此產品，如有連結鼓勵點擊，文末鼓勵按讚留言分享'
 
 print(prompt2)
