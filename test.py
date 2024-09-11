c70ea2f4a2b1feb163f1bfa8c132af09e9628280
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2024-09-11 15:21:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 import random
 import os
 os.system("cls")
-# 定義顏色和數量
-colors = ['白色'] * 6 + ['黑色'] * 6 + ['綠色'] * 5 + ['藍色'] * 5 + ['紅色'] * 5 + ['黃色'] * 5
 
-# 隨機打亂顏色列表
-random.shuffle(colors)
+list = ['1','2','3']
 
-# 打印結果
-print(colors)
+del list[0]
+
+print(list)
