415151668145b9c047f56a6e098fbab3b00ed237
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-03-12 14:25:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-import os
-os.system('cls')
+# group.set_g_pub('place_dict',{random.randint(100000, 999999):'電力',random.randint(100000, 999999):'維生',random.randint(100000, 999999):'任務'},'group',pri_get('g_group'))
+import random
 
-strA='babad'
-strlen=len(strA)
+place_dict={random.randint(100000, 999999):'電力',random.randint(100000, 999999):'維生',random.randint(100000, 999999):'任務'}
+print(place_dict)
+target_value='電力'
 
-for i in range(strlen):
-    print(i)
+key = next((k for k, v in place_dict.items() if v == target_value), None)
+
+print(key)  # ['BBB']
