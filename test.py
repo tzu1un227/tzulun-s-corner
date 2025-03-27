5e3515bf93eb7d6e7feaf7168e925a7af60beea4
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-03-27 11:56:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
-# group.set_g_pub('place_dict',{random.randint(100000, 999999):'電力',random.randint(100000, 999999):'維生',random.randint(100000, 999999):'任務'},'group',pri_get('g_group'))
-import random
+listA=['AAA']
 
-place_dict={random.randint(100000, 999999):'電力',random.randint(100000, 999999):'維生',random.randint(100000, 999999):'任務'}
-print(place_dict)
-target_value='電力'
+listB=listA+['BBB']*3
 
-key = next((k for k, v in place_dict.items() if v == target_value), None)
-
-print(key)  # ['BBB']
+print(listB)
