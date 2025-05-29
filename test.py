be09310d36608a986ebe1e8b59603a5ee1ead59f
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-05-29 13:27:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-listA=['AAA']
+import socket
 
-listB=listA+['BBB']*3
-
-print(listB)
+try:
+    s = socket.socket()
+    s.settimeout(5)  # 最多等 5 秒
+    s.connect(("irl-svr.ee.yzu.edu.tw", 80))
+    print("✅ 連線成功")
+except socket.timeout:
+    print("❌ 連線超時（timeout）")
+except socket.error as e:
+    print(f"❌ 連線失敗：{e}")
+finally:
+    s.close()
