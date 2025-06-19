d32e7378868830990011de1cb3e8e49207971d45
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-06-19 19:43:55 +0800

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -15,12 +15,16 @@ def click_loop():
 def toggle_clicking():
     global clicking
     clicking = not clicking
-    print("連點啟動" if clicking else "連點停止")
+    
+def test_key():
+    print("F8 被偵測到！")
 
 # 開啟點擊執行緒
 threading.Thread(target=click_loop, daemon=True).start()
 
 # 監聽 F8 鍵做啟動/停止
 print("按下 F8 開啟或停止連點，按下 ESC 離開程式")
+
 keyboard.add_hotkey('F8', toggle_clicking)
+keyboard.add_hotkey('F8', test_key)
 keyboard.wait('esc')
