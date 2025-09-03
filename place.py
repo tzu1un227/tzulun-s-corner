ac71c9a8f0ea574a9317d71871c985d803fbe195
tzu1un227 <u8503649@gmail.com>
2025-09-03 15:22:46 +0800

    Create place.py

---
+++ place.py
@@ -0,0 +1,24 @@
+import requests
+
+url = f"https://places.googleapis.com/v1/places:searchNearby?key=AIzaSyBp1b7fufMPm3H0hZUC8pbPGpwoSCvpTDg"
+
+headers = {
+    "Content-Type": "application/json",
+    "X-Goog-FieldMask": "places.id,places.displayName,places.location"
+}
+
+request_body = {
+    "locationRestriction": {
+        "circle": {
+            "center": {
+                "latitude": 25.0330,   # 台北101
+                "longitude": 121.5654
+            },
+            "radius": 500
+        }
+    },
+    "maxResultCount": 5
+}
+
+response = requests.post(url, headers=headers, json=request_body)
+print(response.json())
