49d5185c75b183a3f81623b7392af0964865409b
tzu1un227 <u8503649@gmail.com>
2025-09-22 16:40:30 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,11 +1,7 @@
-nums = [-1,0,1,2,-1,-4]
-result = []
-for x in nums:
-    copy_nums = nums.copy()
-    copy_nums.remove(x)
-    y, z = 0, len(copy_nums) - 1
-    while y != z:
-        if copy_nums[y] + copy_nums[z] == -x:
-            result.append([x, y, z])
+def letterCombinations(digits: str):
 
-print(result)
+    d2l_dict = {"2":"ABC", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs" , "8":"tuv", "9":"wxyz"}
+    for s in digits:
+        d2l_dict[s]
+
+print(letterCombinations("23"))
