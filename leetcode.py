675f7792454e806b129e8d8b82fbccfa13580ce2
tzu1un227 <u8503649@gmail.com>
2025-09-22 12:51:44 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,22 +1,2 @@
-s = "aab"
-p = "c*a*b"
+nums = [-1,0,1,2,-1,-4]
 
-m, n = len(s), len(p)
-dp = [[False] * (n+1) for _ in range(m+1)]
-dp[0][0] = True
-
-for j in range(1,n):
-    if p[j-1]=="*":
-        dp[0][j]=dp[0][j-2]
-
-for i in range(1,m+1):
-    for j in range(1,n+1):
-        if p[j-1]=="*":
-            dp[i][j]=dp[i][j-2]
-            if p[j-2] in [s[i-1],"."]:
-                dp[i][j]=dp[i][j] or dp[i-1][j]
-        elif p[j-1] in [s[i-1],"."]:
-            dp[i][j]=dp[i-1][j-1]
-
-print(dp)
-print(dp[m][n])
