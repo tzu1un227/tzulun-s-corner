8bd8f1c95e3091fcdde29052d5f993aac5c9d9cf
tzu1un227 <u8503649@gmail.com>
2025-09-22 13:50:27 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,2 +1,11 @@
 nums = [-1,0,1,2,-1,-4]
+result = []
+for x in nums:
+    copy_nums = nums.copy()
+    copy_nums.remove(x)
+    y, z = 0, len(copy_nums) - 1
+    while y != z:
+        if copy_nums[y] + copy_nums[z] == -x:
+            result.append([x, y, z])
 
+print(result)
