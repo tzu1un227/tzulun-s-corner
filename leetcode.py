8e61307356a71ca911f9d20e9ee1af466859f898
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-09-22 22:12:54 +0800

    Update leetcode.py

--- leetcode.py
+++
@@ -1,7 +0,0 @@
-def letterCombinations(digits: str):
-
-    d2l_dict = {"2":"ABC", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs" , "8":"tuv", "9":"wxyz"}
-    for s in digits:
-        d2l_dict[s]
-
-print(letterCombinations("23"))
