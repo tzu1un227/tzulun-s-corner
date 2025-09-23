09802225767dc8edb6eb735b5781f3f727636f22
tzu1un227 <u8503649@gmail.com>
2025-09-23 12:07:35 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,7 +1,22 @@
-def letterCombinations(digits: str):
+n = 3
+parentheses_list = ["()"] * n*2
+result_list = []
+temp = []
 
-    d2l_dict = {"2":"ABC", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs" , "8":"tuv", "9":"wxyz"}
-    for s in digits:
-        d2l_dict[s]
+def BT(index):
+    if len(temp) == n * 2:
+        result_list.append("".join(temp))
+        return
+    
+    for c in parentheses_list[index]:
+        if (temp.count("(") == temp.count(")") and c == ")") or (temp.count("(") == n and c == "("):
+            continue
 
-print(letterCombinations("23"))
+        temp.append(c)
+        BT(index+1)
+        temp.pop()
+
+BT(0)
+
+print(parentheses_list)
+print(result_list)
