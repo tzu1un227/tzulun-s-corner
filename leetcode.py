7519a6ce9332cce47a5dc3444ee06f12134f5abc
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-09-24 23:01:37 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,14 @@
-def jump(nums):
+def canJump(nums) -> bool:
+    currEnd = 0
+    for i in range(len(nums)):
+        if currEnd >= len(nums) -1 :
+            return True
+        if i > currEnd:
+            return False
+        if i+nums[i] >= currEnd:
+            currEnd = i+nums[i]
 
-    dp = [len(nums)] * len(nums)
-    dp[0] = 0
-    
-    for i in range(0, len(nums) - 1):
-        for j in range(1, nums[i]+1):
-            if i + j >= len(nums) :
-                continue
-            if dp[i+j] > dp[i] + 1:
-                dp[i+j] = dp[i] + 1
+    return False
 
-    return dp[-1]
 
-nums = [1,3,2]
-print(jump(nums))
+print(canJump([2,5,0,0]))
