851bc04e391a92b8465faa2b48a556b97097d59f
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-09-27 20:10:59 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,12 +1,14 @@
-def mergeTwoLists(list1, list2):
-    result = []
-    while list1 or list2:
-        if not list1 or list1[0] > list2[0]:
-            result.append(list2[0])
-            list2.pop(0)
-        elif not list2 or list1[0] <= list2[0]:
-            result.append(list1[0])
-            list1.pop(0)
-    return result
+def findMedianSortedArrays(nums1, nums2):
+    MedianIndex = (len(nums1) + len(nums2)) // 2
+    sorted_nums = []
+    while nums1 and nums2:
+        if nums1[0] > nums2[0]:
+            sorted_nums.append(nums2[0])
+            nums2.pop(0)
+        else:
+            sorted_nums.append(nums1[0])
+            nums1.pop(0)
+    sorted_nums += nums1 if nums1 else nums2
+    return float(sorted_nums[MedianIndex]) if len(sorted_nums) % 2 != 0 else (float(sorted_nums[MedianIndex]) + float(sorted_nums[MedianIndex-1])) / 2.0
 
-print(mergeTwoLists(list1 = [1,2,4], list2 = [1,3,4]))
+print(findMedianSortedArrays([1,2,3,4,5], [6,7,8,9,10,11,12,13,14,15,16,17]))
