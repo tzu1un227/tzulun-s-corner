1ba042d29c1599eb62fb16ccf0992755df01b8c3
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-09-29 15:02:30 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,14 +1,9 @@
-def findMedianSortedArrays(nums1, nums2):
-    MedianIndex = (len(nums1) + len(nums2)) // 2
-    sorted_nums = []
-    while nums1 and nums2:
-        if nums1[0] > nums2[0]:
-            sorted_nums.append(nums2[0])
-            nums2.pop(0)
-        else:
-            sorted_nums.append(nums1[0])
-            nums1.pop(0)
-    sorted_nums += nums1 if nums1 else nums2
-    return float(sorted_nums[MedianIndex]) if len(sorted_nums) % 2 != 0 else (float(sorted_nums[MedianIndex]) + float(sorted_nums[MedianIndex-1])) / 2.0
+def minScoreTriangulation(values):
+    dp = [0] * (len(values) - 2)
+    dp[0] = values[0] * values[1] * values[2]
+    for i in range(1,len(values)-2):
+        dp[i] = dp[i-1] + min()
+    return dp[-1]
 
-print(findMedianSortedArrays([1,2,3,4,5], [6,7,8,9,10,11,12,13,14,15,16,17]))
+
+print(minScoreTriangulation(values = [4,3,4,3,5]))
