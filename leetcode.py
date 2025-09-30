af636e05f1c76a71f96e6f446bfc15949d09e7ef
tzu1un227 <u8503649@gmail.com>
2025-09-30 14:45:42 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,25 +1,7 @@
-def triangleNumber(nums):
-    nums = sorted(nums,reverse=1)
-    result = 0
-    for i in range(len(nums) - 2):
-        if nums[i] > 0:
-            for j in range(i+1, len(nums) - 1):
-                left = j + 1
-                right = len(nums) - 1
-                while left < right:
-                    mid = (left + right) // 2
-                    if nums[mid] + nums[j] <= nums[i]:
-                        right = mid - 1
-                    elif nums[mid] + nums[j] > nums[i]:
-                        left = mid + 1
-                mid = max(left, right)
-                if mid < len(nums)-1 and nums[mid + 1] + nums[j] > nums[i]:
-                    result += mid - j + 1
-                elif nums[mid] + nums[j] > nums[i]:
-                    result += mid - j
-                else:
-                    result += mid - j - 1
-        else:
-            continue
-    return result
-print(triangleNumber([7,0,0,0]))
+def triangularSum(nums):
+    if len(nums) == 1:
+        return nums[0]
+    for i in range(len(nums)-1):
+            nums[i]=nums[i]+nums[i+1]
+    return triangularSum(nums[:-1]) % 10
+print(triangularSum([1,2,3,4,5]))
