dd62cf3b41db3a913c06dcd6074fb89200589918
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2025-10-12 14:07:53 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,9 @@
-def minPathSum(grid):
-    n = len(grid[0])
-    m = len(grid)
-    dp = [[0 for _ in range(n)] for _ in range(m)]
-    for i in range(0, m):
-        for j in range(0, n):
-            if i == 0 and j ==0:
-                dp[i][j] = grid[i][j]
-            elif i == 0:
-                dp[i][j] = dp[i][j-1] + grid[i][j]
-            elif j == 0:
-                dp[i][j] = dp[i-1][j] + grid[i][j]
-            else:
-                dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j]
-            
-    return dp[-1][-1]
-print(minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+def maximumTotalDamage(power):
+    power_sorted = sorted(power)
+    power_set_list = list(set(power_sorted))
+    dp = [0] * len(power_set_list)
+    for i in range(len(power_set_list)):
+        dp[i] = sum([j for j in power_sorted if abs(j-power_set_list[i])==0 or abs(j-power_set_list[i]) > 2])
+
+    return max(dp)
+print(maximumTotalDamage([1,1,3,4]))
