35a8eb044c02e186d3c489fc553b782528ea3bf6
tzu1un227 <139104101+tzu1un227@users.noreply.github.com>
2026-04-09 12:44:46 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,8 @@
 def topKFrequent(nums,k):
-    setOfNum=set(nums)
+    fdict={}
+    for num in nums:
+        fdict[num]=fdict.get(num,0)+1
+
+    return sorted(fdict.items(),key=lambda x: x[1], reverse=True)[:k]
     
 print(topKFrequent(nums = [1,2,2,3,3,3], k = 2))
